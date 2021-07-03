8decf9be63d05bdf06d967c500196cd6bc041316
Marco Vincit <contato@marcovincit.com>
2021-07-03 13:14:32 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useState, useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [background, setBackground] = useState(0);
+  const [color, setColor] = useState(0);
+
+  useEffect(() => {
+    const min = 1;
+    const max = 360;
+
+    setBackground(min + Math.random() * (max - min));
+    setColor(min + Math.random() * (max - min));
+
+    const doc = document.documentElement;
+    doc.style.setProperty("--text-color", `hsl(${color}, 30%, 70%)`);
+    doc.style.setProperty("--background-color", `hsl(${background}, 50%, 10%)`);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
