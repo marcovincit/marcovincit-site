d0d5fb81b27b41a09e8ed9ad2b143df4833ca13e
Marco Vincit <marcovincit@gmail.com>
2022-03-25 20:32:57 +0000

    Update 14 files

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,23 +1,10 @@
 import styles from "./Header.module.scss";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useSite } from "context";
 
-export default function Header() {
-  const { setPageState } = useSite();
-  const router = useRouter();
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    router.push("/");
-
-    setPageState({});
-  };
-
+export function Header() {
   return (
     <header className={styles.container}>
       <h1>
-        <a onClick={handleClick}>Marco Vincit</a>
+        <a>Marco Vincit</a>
       </h1>
       <div className={styles.content}>
         <h2>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import styles from "styles/Home.module.scss";
+import data from "data/about";
+import parse from "html-react-parser";
+
+import { Header, Menu, Footer } from "components";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Footer />
+    </>
+  );
 }
