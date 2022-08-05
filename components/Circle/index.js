c38d1731d555bdec3b7698b7f563acff51b05b5c
Marco Vincit <contato@marcovincit.com>
2022-08-04 22:24:05 -0300

    .

--- components/Circle/index.js
+++
@@ -1,8 +0,0 @@
-import styles from "./Circle.module.scss";
-
-export default function Circle(props) {
-  const { as, children } = props;
-  const TagName = as ? as : "div";
-
-  return <TagName className={styles.container}>{children}</TagName>;
-}
--- components/Grid/index.js
+++
@@ -1,8 +0,0 @@
-import styles from "./Grid.module.scss";
-
-export default function Grid(props) {
-  const { as, children, col } = props;
-  const TagName = as ? as : "div";
-
-  return <TagName className={styles.grid}>{children}</TagName>;
-}
